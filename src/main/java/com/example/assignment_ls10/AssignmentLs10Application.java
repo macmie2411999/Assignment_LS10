ed4da0f8885c67db6f69c4184ca3764510e02fa6
TênMac <caoducmanh2411999@gmail.com>
2022-07-12 18:17:31 +0300

    - By Mac Mie

---
+++ src/main/java/com/example/assignment_ls10/AssignmentLs10Application.java
@@ -0,0 +1,13 @@
+package com.example.assignment_ls10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentLs10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentLs10Application.class, args);
+    }
+
+}
